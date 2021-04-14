001805bc708b51b8abe76abbf2614976622e0d69
Sally <s.a.m.hogenboom@uva.nl>
2021-04-14 13:56:07 +0200

    Add stimuli

---
+++ stimuli.js
@@ -0,0 +1,147 @@
+var target_stimuli = [
+{trial_target: 'host', 
+trial_condition: 'positive'}, 
+{trial_target: 'host', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'comedian', 
+trial_condition: 'positive'}, 
+{trial_target: 'comedian', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'model', 
+trial_condition: 'positive'}, 
+{trial_target: 'model', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'babysitter', 
+trial_condition: 'positive'}, 
+{trial_target: 'babysitter', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'dancer', 
+trial_condition: 'positive'}, 
+{trial_target: 'dancer', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'mechanic', 
+trial_condition: 'positive'}, 
+{trial_target: 'mechanic', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'tutor', 
+trial_condition: 'positive'}, 
+{trial_target: 'tutor', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'waiter', 
+trial_condition: 'positive'}, 
+{trial_target: 'waiter', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'waitress', 
+trial_condition: 'positive'}, 
+{trial_target: 'waitress', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'agent', 
+trial_condition: 'positive'}, 
+{trial_target: 'agent', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'astronaut', 
+trial_condition: 'positive'}, 
+{trial_target: 'astronaut', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'athlete', 
+trial_condition: 'positive'}, 
+{trial_target: 'athlete', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'composer', 
+trial_condition: 'positive'}, 
+{trial_target: 'composer', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'explorer', 
+trial_condition: 'positive'}, 
+{trial_target: 'explorer', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'scientist', 
+trial_condition: 'positive'}, 
+{trial_target: 'scientist', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'developer', 
+trial_condition: 'positive'}, 
+{trial_target: 'developer', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'entrepreneur', 
+trial_condition: 'positive'}, 
+{trial_target: 'entrepreneur', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'intern', 
+trial_condition: 'positive'}, 
+{trial_target: 'intern', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'mayor', 
+trial_condition: 'positive'}, 
+{trial_target: 'mayor', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'technician', 
+trial_condition: 'positive'}, 
+{trial_target: 'technician', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'assistant', 
+trial_condition: 'positive'}, 
+{trial_target: 'assistant', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'librarian', 
+trial_condition: 'positive'}, 
+{trial_target: 'librarian', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'painter', 
+trial_condition: 'positive'}, 
+{trial_target: 'painter', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'writer', 
+trial_condition: 'positive'}, 
+{trial_target: 'writer', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'contractor', 
+trial_condition: 'positive'}, 
+{trial_target: 'contractor', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'editor', 
+trial_condition: 'positive'}, 
+{trial_target: 'editor', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'psychologist', 
+trial_condition: 'positive'}, 
+{trial_target: 'psychologist', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'publisher', 
+trial_condition: 'positive'}, 
+{trial_target: 'publisher', 
+trial_conditaion: 'negative'}, 
+
+{trial_target: 'surgeon', 
+trial_condition: 'positive'}, 
+{trial_target: 'surgeon', 
+trial_conditaion: 'negative'}, 
+
+];
